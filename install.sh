cbf6696802e52c74d98d8e592c7c0238b4139ccd
VyTale <vincentkellyb@gmail.com>
2024-08-22 17:37:34 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+
+if [ ! -f ~/.cache/oh-my-posh/themes/custom-theme.omp.json ]; then
+    curl -s https://ohmyposh.dev/install.sh | bash -s
+    curl -s https://raw.githubusercontent.com/vytale/install-custom-prompt/main/custom-theme.omp.json >> ~/.cache/oh-my-posh/themes/custom-theme.omp.json
+fi
+eval "$(oh-my-posh init bash --config ~/.cache/oh-my-posh/themes/custom-theme.omp.json)"
